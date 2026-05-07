4a7e4aec4abd9365a5be2136c06ebd5c4b1df997
Akil1928 <akilwatsonch1928@gmail.com>
2026-05-06 18:15:21 -0600

    mas clases

---
+++ src/main/java/model/Stack/ArrayStack.java
@@ -0,0 +1,55 @@
+package model.Stack;
+
+public class ArrayStack<T> implements MyStack {
+
+    private int n;
+    private int top;
+    private T[] data;
+
+    public ArrayStack(int n) {
+        if (n <= 0) System.exit(1);
+        this.n = n;
+        this.top = -1;//Fuera de cualquier indice del arreglo
+        data = (T[]) new Object[n];
+    }
+
+
+    @Override
+    public int size() {
+        return top + 1;
+    }
+
+    @Override
+    public void clear() {
+        this.top = -1;//Fuera de cualquier indice del arreglo
+        data = (T[]) new Object[n];
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return top == -1;
+    }
+
+    @Override
+    public T peek() throws StackException {
+        if (isEmpty()) throw new StackException("Array Stack is empty");
+        return this.data[top];
+    }
+
+    @Override
+    public T top() throws StackException {
+        if (isEmpty()) throw new StackException("Array Stack is empty");
+        return this.data[top];
+    }
+
+    @Override
+    public void push(Object element) throws StackException {
+        if (top == n - 1) throw new StackException("Array Stack is full");
+
+    }
+
+    @Override
+    public Object pop() throws StackException {
+        return null;
+    }
+}
